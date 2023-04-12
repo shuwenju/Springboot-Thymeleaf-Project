474425c9d473bba9f87f40a00d592df45376086c
mrabi24 <120145506+mrabi24@users.noreply.github.com>
2023-04-11 22:15:42 -0400

    landing page

--- project/src/main/java/com/jac/thymeleaf/thymeleaf/controller/DemoController.java
+++ project/src/main/java/com/jac/thymeleaf/thymeleaf/controller/DemoController.java
@@ -1,2 +1,14 @@
-package com.jac.thymeleaf.thymeleaf.controller;public class DemoController {
-}
+package com.jac.thymeleaf.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController
+    {
+        @GetMapping("/")
+        public String landingpage(Model theModel) {
+            return "landingpage";
+        }
+    }
