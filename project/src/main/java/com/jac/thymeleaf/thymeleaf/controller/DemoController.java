efd87a936bf5fd75339677a50743071c228df338
mrabi24 <120145506+mrabi24@users.noreply.github.com>
2023-04-14 18:55:43 -0400

    implemented sessions (styling needs to be done)

--- project/src/main/java/com/jac/thymeleaf/thymeleaf/controller/DemoController.java
+++ project/src/main/java/com/jac/thymeleaf/thymeleaf/controller/DemoController.java
@@ -2,23 +2,30 @@ package com.jac.thymeleaf.thymeleaf.controller;
 
 import com.jac.thymeleaf.thymeleaf.entity.UserEntity;
 import com.jac.thymeleaf.thymeleaf.mapper.MapperHelper;
+import com.jac.thymeleaf.thymeleaf.model.LoginFormModel;
 import com.jac.thymeleaf.thymeleaf.model.UserModel;
+import com.jac.thymeleaf.thymeleaf.repository.UserRepository;
 import com.jac.thymeleaf.thymeleaf.service.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/social")
 public class DemoController {
         private final UserService userService;
         private final MapperHelper mapper;
+
+        @Autowired
+        private UserRepository userRepository;
         @Autowired
         public DemoController(UserService userService, MapperHelper mapper) {
             this.userService = userService;
@@ -50,6 +57,39 @@ public class DemoController {
 //            model.addAttribute("successMessage", "Registration successful. You can log in now.");
 //            model.addAttribute("userModel", new UserModel());
             return "login";
+        }
+
+        @GetMapping("/login")
+        public String showLoginPage(Model model) {
+            model.addAttribute("loginForm", new LoginFormModel());
+            return "login";
+        }
+
+        @PostMapping("/login")
+        public String processLoginForm(@Valid @ModelAttribute("loginForm") LoginFormModel loginForm,
+                                       BindingResult bindingResult, Model model, HttpServletRequest request) {
+            if (bindingResult.hasErrors()) {
+                return "login";
+            }
+
+            Optional<UserEntity> optionalUser = userRepository.findByEmail(loginForm.getEmail());
+
+            if (!optionalUser.isPresent()) {
+                model.addAttribute("errorMessage", "Invalid email or password");
+                return "login";
+            }
+
+            UserEntity user = optionalUser.get();
+
+            if (!user.getPassword().equals(loginForm.getPassword())) {
+                model.addAttribute("errorMessage", "Invalid email or password");
+                return "login";
+            }
+
+            // Login successful, set user in session and redirect to dashboard
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            return "redirect:/newsfeed";
         }
 
 
---
+++ project/src/main/java/com/jac/thymeleaf/thymeleaf/model/LoginFormModel.java
@@ -0,0 +1,19 @@
+package com.jac.thymeleaf.thymeleaf.model;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class LoginFormModel {
+    @Email(message = "Please enter a valid email address")
+    private String email;
+
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+}
