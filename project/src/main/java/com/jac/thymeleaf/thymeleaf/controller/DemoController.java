51e3e6bab2eb034c7a3d3ee987c26b5fb2a9468a
mrabi24 <120145506+mrabi24@users.noreply.github.com>
2023-04-17 15:26:18 -0400

    fixed redirect register/login

--- project/src/main/java/com/jac/thymeleaf/thymeleaf/controller/DemoController.java
+++ project/src/main/java/com/jac/thymeleaf/thymeleaf/controller/DemoController.java
@@ -62,16 +62,17 @@ public class DemoController {
         public String register(@ModelAttribute("userModel") @Valid UserModel userModel, BindingResult result, Model model) {
             if (result.hasErrors()) {
 //                model.addAttribute("registrationError");
-                return "register";
+                return "redirect:/social/register";
             }else {
 
                 UserEntity userEntity = mapper.convertUserModeltoEntity(userModel);
                 userService.save(userEntity);
+                return "redirect:/social/login";
             }
 
 //            model.addAttribute("successMessage", "Registration successful. You can log in now.");
 //            model.addAttribute("userModel", new UserModel());
-            return "login";
+
 
         }
 
@@ -160,4 +161,12 @@ public class DemoController {
         mediaService.savePost(newPost);
         return "redirect:newsfeed";
     }
+
+
+    @PostMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login";
+    }
+
 }
